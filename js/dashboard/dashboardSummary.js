55b9a69d311a483184588aa396e381328e2b97f8
Alex Kagadovsky <a.kagadovskiy@gmail.com>
2015-09-16 14:35:19 +0000

    roles hack

--- js/dashboard/dashboardSummary.js
+++ js/dashboard/dashboardSummary.js
@@ -5,7 +5,7 @@ var oneMonthAgo = moment().subtract(1, 'months').toDate();
 
 var has_seen_alert = false;
 
-function dashboardSummary(Restangular) {
+function dashboardSummary(Restangular, $window) {
     'use strict';
 
     return {
@@ -14,38 +14,41 @@ function dashboardSummary(Restangular) {
         controller: function($scope) {
             $scope.stats = {};
             $scope.has_seen_alert = has_seen_alert;
+            $scope.isAdmin = $window.localStorage.getItem('posters_galore_login') == 'admin';
             $scope.dismissAlert = () => {
                 has_seen_alert = true;
                 $scope.has_seen_alert = true;
             };
-            Restangular
-                .all('commands')
-                .getList({filter: '{"date_gte":"' + oneMonthAgo.toISOString() +'"}', sort: '["date","DESC"]'})
-                .then(commands => {
-                    $scope.stats.commands = commands.data
-                        .reduce((stats, command) => {
-                            if (command.status != 'cancelled') stats.revenue += command.total;
-                            if (command.status == 'ordered') stats.pending_orders++;
-                            return stats;
-                        }, { revenue: 0, pending_orders: 0})
-                });
+            if ($scope.isAdmin) {
+                Restangular
+                    .all('commands')
+                    .getList({filter: '{"date_gte":"' + oneMonthAgo.toISOString() +'"}', sort: '["date","DESC"]'})
+                    .then(commands => {
+                        $scope.stats.commands = commands.data
+                            .reduce((stats, command) => {
+                                if (command.status != 'cancelled') stats.revenue += command.total;
+                                if (command.status == 'ordered') stats.pending_orders++;
+                                return stats;
+                            }, { revenue: 0, pending_orders: 0})
+                    });
+                Restangular
+                    .all('reviews')
+                    .getList({range: '[1,100]', sort: '["date","DESC"]', filter: '{"status":"pending"}'})
+                    .then(reviews => {
+                        $scope.stats.reviews = reviews.data.reduce(nb => ++nb, 0)
+                    });
+            }                
             Restangular
                 .all('customers')
                 .getList({range: '[1,100]', sort: '["first_seen","DESC"]', filter: '{"has_ordered":true,"first_seen_gte":"' +oneMonthAgo.toISOString() + '"}'})
                 .then(customers => {
                     $scope.stats.customers = customers.data.reduce(nb => ++nb, 0)
                 });
-            Restangular
-                .all('reviews')
-                .getList({range: '[1,100]', sort: '["date","DESC"]', filter: '{"status":"pending"}'})
-                .then(reviews => {
-                    $scope.stats.reviews = reviews.data.reduce(nb => ++nb, 0)
-                });
         },
         template: dashboardSummaryTemplate
     };
 }
 
-dashboardSummary.$inject = ['Restangular'];
+dashboardSummary.$inject = ['Restangular', '$window'];
 
 export default dashboardSummary;
--- js/main.js
+++ js/main.js
@@ -29,29 +29,49 @@ myApp.controller('username', ['$scope', '$window', function($scope, $window) { /
 // custom states (pages)
 myApp.config(['$stateProvider', require('./segments/segmentsState')]);
 
-myApp.config(['NgAdminConfigurationProvider', function (nga) {
+myApp.config(['NgAdminConfigurationProvider', '$windowProvider', function (nga, $window) {
     // create the admin application
     var admin = nga.application('My First Admin')
         .baseApiUrl('/');
 
-    // add entities
-    admin.addEntity(nga.entity('customers'));
-    admin.addEntity(nga.entity('categories'));
-    admin.addEntity(nga.entity('products'));
-    admin.addEntity(nga.entity('reviews'));
-    admin.addEntity(nga.entity('commands'));
+    if ($window.$get().localStorage.getItem('posters_galore_login') == 'admin') { //retrieving current role
+        // add entities
+        admin.addEntity(nga.entity('customers'));
+        admin.addEntity(nga.entity('categories'));
+        admin.addEntity(nga.entity('products'));
+        admin.addEntity(nga.entity('reviews'));
+        admin.addEntity(nga.entity('commands'));
 
-    // configure entities
-    require('./customers/config')(nga, admin);
-    require('./categories/config')(nga, admin);
-    require('./products/config')(nga, admin);
-    require('./reviews/config')(nga, admin);
-    require('./commands/config')(nga, admin);
+        // configure entities
+        require('./customers/config')(nga, admin);
+        require('./categories/config')(nga, admin);
+        require('./products/config')(nga, admin);
+        require('./reviews/config')(nga, admin);
+        require('./commands/config')(nga, admin);
 
-    admin.dashboard(require('./dashboard/config')(nga, admin));
-    admin.header(require('./header.html'));
-    admin.menu(require('./menu')(nga, admin));
+        admin.dashboard(require('./dashboard/config')(nga, admin));
+        admin.header(require('./header.html'));
+        admin.menu(require('./menu')(nga, admin));
 
-    // attach the admin application to the DOM and execute it
-    nga.configure(admin);
+        // attach the admin application to the DOM and execute it
+        nga.configure(admin);
+    } else {
+        // add entities
+        admin.addEntity(nga.entity('customers').readOnly());
+        admin.addEntity(nga.entity('categories').readOnly());
+        admin.addEntity(nga.entity('products').readOnly());
+
+        // configure entities
+        require('./customers/userConfig')(nga, admin);
+        require('./categories/userConfig')(nga, admin);
+        require('./products/userConfig')(nga, admin);
+
+        admin.dashboard(require('./dashboard/userConfig')(nga, admin, $window.$get()));
+        admin.header(require('./header.html'));
+        admin.menu(require('./userMenu')(nga, admin));
+
+        // attach the admin application to the DOM and execute it
+        nga.configure(admin);
+    }
+
 }]);
