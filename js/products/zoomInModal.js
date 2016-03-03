be8b3cdc54ffe0d98ced697ad1a6285bedd68a4b
alexisjanvier <contact@alexisjanvier.net>
2016-03-03 12:57:26 +0100

    Fix modal

--- js/products/zoomInModal.js
+++ js/products/zoomInModal.js
@@ -1,4 +1,4 @@
-const zoomInModal = ($modal) => {
+const zoomInModal = ($uibModal) => {
     return {
         restrict: 'E',
         scope: {
@@ -8,16 +8,16 @@ const zoomInModal = ($modal) => {
         link: (scope) => {
             scope.zoomThumbnail = ($event) => {
                 $event.preventDefault();
-                $modal.open({
+                $uibModal.open({
                     backdrop: true,
                     scope: scope,
-                    controller: ['$scope', '$modalInstance', ($scope, $modalInstance) => {
-                        $scope.close = () => $modalInstance.close();
+                    controller: ['$scope', '$uibModalInstance', ($scope, $uibModalInstance) => {
+                        $scope.close = () => $uibModalInstance.close();
                     }],
                     template:
                         `<div class="modal-header"><h4>Zoom</h4></div>
                         <div class="modal-body">
-                            <img src="{{ image }}" class="img-thumbnail img-responsive"/>
+                            <img ng-src="{{ image }}" class="img-thumbnail img-responsive"/>
                         </div>
                         <div class="modal-footer">
                             <button ng-click="close()" class="btn btn-success" >
@@ -28,10 +28,10 @@ const zoomInModal = ($modal) => {
             }
         },
         template:
-            `<img src="{{ thumbnail }}" class="poster_mini_thumbnail" ng-click="zoomThumbnail($event, image)"/>`
+            `<img src="{{ thumbnail }}" class="poster_mini_thumbnail" ng-click="zoomThumbnail($event)"/>`
     };
 }
 
-zoomInModal.$inject = ['$modal'];
+zoomInModal.$inject = ['$uibModal'];
 
 export default zoomInModal;
