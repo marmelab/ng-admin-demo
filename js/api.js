b57e65d8dcc6aed2faa5c557aea268041c42163e
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2016-09-13 14:38:49 +0200

    Fix latest dependencies issues

--- js/api.js
+++ js/api.js
@@ -1,17 +1,24 @@
-var FakeRest = require('fakerest');
-var sinon = require('sinon/lib/sinon/util/fake_server');
+import FakeRest from 'fakerest';
+
+import {
+    fakeServer,
+    FakeXMLHttpRequest,
+    useFakeXMLHttpRequest
+} from 'sinon';
+
 
 var restServer = new FakeRest.Server();
 restServer.toggleLogging();
 restServer.init(data);
 
-sinon.FakeXMLHttpRequest.useFilters = true;
-sinon.FakeXMLHttpRequest.addFilter(function (method, url) {
+FakeXMLHttpRequest.useFilters = true;
+FakeXMLHttpRequest.addFilter(function (method, url) {
     // Do not catch webpack sync, config.js transformation but catch /upload in test env
     return url.indexOf('/socket.io/') !== -1 || url.indexOf('config.js') !== -1;
 });
+useFakeXMLHttpRequest();
 
-var server = sinon.fakeServer.create();
-server.autoRespond = true;
-server.autoRespondAfter = 0; // answer immediately
-server.respondWith(restServer.getHandler());
+var server = fakeServer.create();
+fakeServer.autoRespond = true;
+fakeServer.autoRespondAfter = 0; // answer immediately
+fakeServer.respondWith(restServer.getHandler());
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
 
 module.exports = {
-    entry: ["./js/main.js", "./css/main.scss"],
+    entry: [
+        './js/main.js',
+        // './css/main.scss',
+    ],
     output: {
         path: __dirname + "/build",
         filename: "main.js",
@@ -10,16 +14,18 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.css$/, loader: "style!css" },
-            { test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel' },
-            { test: /node_modules[\\\/]admin-config[\\\/].*\.jsx?$/, loader: 'babel' },
+            { test: /\.js$/, loader: 'babel' },
             { test: /\.html$/, loader: 'html' },
             { test: /\.css$/, loader: ExtractTextPlugin.extract('css') },
             { test: /\.scss$/, loader: ExtractTextPlugin.extract('css!sass') }
-        ]
+        ],
     },
     plugins: [
         new ExtractTextPlugin('[name].css', {
             allChunks: true
         })
-    ]
+    ],
+    externals: {
+        'text-encoding': 'window'
+    }
 };
